4121aca4544b096583e0aca9539b7deff38b413f
Emmanuelabn <emmanuel.abdelnour@net.usj.edu.lb>
2023-03-25 13:53:08 +0200

    API uploaded

---
+++ DataContract/Request/EmergencyContactRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DatabaseScaffolding.Model;
+
+namespace DataContract.Request
+{
+    public class EmergencyContactRequest
+    {
+        public Guid EmergencyContactId { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsUser { get; set; }
+        public bool IsDisabled { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
