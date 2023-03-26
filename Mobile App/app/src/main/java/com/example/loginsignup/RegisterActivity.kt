8e5791ba77e8c88d6aa95abaa274e3c2df6ae51e
Anthony Mghames <anthonymghames0@gmail.com>
2023-03-26 17:36:56 +0300

    Fixed colors and hints

--- Mobile App/app/src/main/java/com/example/loginsignup/RegisterActivity.kt
+++ Mobile App/app/src/main/java/com/example/loginsignup/RegisterActivity.kt
@@ -8,19 +8,23 @@ import com.example.loginsignup.databinding.ActivityRegisterBinding
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnRegister.setOnClickListener {
-            startActivity(Intent(this,LoginActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
         }
 
         binding.tvHaveAccount.setOnClickListener {
-            startActivity(Intent(this,LoginActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
         }
 
     }
+
+
+
+
+
 }
