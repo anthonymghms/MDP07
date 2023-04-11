ff983310398327ed994f5731f8113a81d24f9ed7
Anthony Mghames <anthonymghames0@gmail.com>
2023-04-12 01:05:56 +0300

    Changes

---
+++ Mobile App/app/src/main/java/com/example/roadguard/authentication/ForgotPasswordActivity.kt
@@ -0,0 +1,67 @@
+package com.example.roadguard.authentication
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.roadguard.client.HTTPRequest
+import com.example.roadguard.client.ResponseCallback
+import com.example.roadguard.databinding.ActivityForgotPasswordBinding
+
+class ForgotPasswordActivity : AppCompatActivity(), ResponseCallback {
+
+    private lateinit var binding: ActivityForgotPasswordBinding
+    private var client = HTTPRequest()
+    private var uri: Uri? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        uri = intent.data;
+
+        if (uri!=null) {
+            resetPassword(uri!!)
+        }
+
+        binding.btnSubmit.setOnClickListener {
+            val newPassword = binding.etNewPassword.text.toString().trim()
+            val confirmPassword = binding.etConfirmPassword.text.toString().trim()
+
+            if (newPassword.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                if (newPassword == confirmPassword) {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    Toast.makeText(this, "Password reset successfully.", Toast.LENGTH_LONG).show()
+                    finish()
+                } else {
+                    Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Please fill in all fields.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun resetPassword(uri: Uri){
+        val token = uri.getQueryParameter("token")
+        val email = uri.getQueryParameter("email")
+        val url = "https://roadguard.azurewebsites.net/api/auth/resetpassword"
+        val queryParams = mutableMapOf<String, String>()
+        token?.let { queryParams["token"] = it }
+        email?.let { queryParams["email"] = it }
+
+        client.post(this,url,"{}" ,this,queryParams )
+
+    }
+
+    override fun onSuccess(response: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFailure(error: Throwable) {
+        error.printStackTrace()
+    }
+}
--- Mobile App/app/src/main/java/com/example/roadguard/authentication/OtpActivity.kt
+++ Mobile App/app/src/main/java/com/example/roadguard/authentication/OtpActivity.kt
@@ -1,20 +1,26 @@
 package com.example.roadguard.authentication
 
+import android.content.Intent
 import android.text.Editable
 import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
+import com.example.roadguard.HomeActivity
 import com.example.roadguard.R
 import com.example.roadguard.client.HTTPRequest
 import com.example.roadguard.client.ResponseCallback
+import com.example.roadguard.sharedPrefs.SharedPrefsHelper
+import org.json.JSONObject
 
 class OtpActivity : AppCompatActivity(), ResponseCallback {
 
     private val otpEditTexts = arrayOfNulls<EditText>(6)
     private var client: HTTPRequest = HTTPRequest()
     private lateinit var tvError: TextView
+    private lateinit var username: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,11 @@ class OtpActivity : AppCompatActivity(), ResponseCallback {
         otpEditTexts[3] = findViewById(R.id.et_otp_4)
         otpEditTexts[4] = findViewById(R.id.et_otp_5)
         otpEditTexts[5] = findViewById(R.id.et_otp_6)
+
+
+
+        tvError = findViewById(R.id.tv_otp_error)
+        username = intent.getStringExtra("username").toString()
 
         for (i in otpEditTexts.indices) {
             otpEditTexts[i]?.addTextChangedListener(createTextWatcher(i))
@@ -42,7 +53,12 @@ class OtpActivity : AppCompatActivity(), ResponseCallback {
                 }
 
                 if (index == otpEditTexts.size - 1 && count == 1) {
-                    submitOtp()
+                    val otp = StringBuilder()
+                    for (editText in otpEditTexts) {
+                        otp.append(editText?.text.toString())
+                    }
+                    val jsonBody = "{\"username\":\"$username\",\"otp\":\"$otp\"}"
+                    client.post(this@OtpActivity,"https://roadguard.azurewebsites.net/api/auth/login-2fa",jsonBody,this@OtpActivity)
                 }
             }
 
@@ -50,18 +66,30 @@ class OtpActivity : AppCompatActivity(), ResponseCallback {
         }
     }
 
-    private fun submitOtp() {
-        val otp = StringBuilder()
-        for (editText in otpEditTexts) {
-            otp.append(editText?.text.toString())
+    override fun onSuccess(response: String) {
+        tvError = findViewById(R.id.tv_otp_error)
+        val jsonObject = JSONObject(response)
+        Log.d("Response", response)
+        jsonObject.keys().forEach {
+            when (it) {
+                "token" -> {
+                    val token = jsonObject.getString(it)
+                    SharedPrefsHelper.saveToken(this, token)
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                }
+                "message" -> {
+                    runOnUiThread {
+                        tvError.visibility = android.view.View.VISIBLE
+                        tvError.text = jsonObject.getString(it)
+                    }
+                }
+
+            }
         }
     }
 
-    override fun onSuccess(response: String) {
-        TODO("Not yet implemented")
-    }
-
     override fun onFailure(error: Throwable) {
-        TODO("Not yet implemented")
+        error.printStackTrace()
     }
 }
