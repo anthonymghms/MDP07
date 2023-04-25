aea97bb6554dc808959e1fd4f159bf204c6bb59a
Mario Francis <mario.francis@net.usj.edu.lb>
2023-04-25 17:19:51 +0300

    created backoffice

---
+++ API/BackOffice/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using DatabaseMigration;
+using DatabaseMigration.Model;
+
+namespace BackOffice.Repositories
+{
+	public class UserRepository
+	{
+		private readonly DrowsinessDetectionContext _context;
+		public UserRepository(DrowsinessDetectionContext context)
+		{
+			_context = context;
+		}
+		public IEnumerable<AppUser> GetUsers()
+		{
+			return _context.Users.ToList();
+		}
+	}
+}
+
